0ae400ebda586ff3a230cdcd0a379b6c9b543d6a
Thiago Marçal <thiagormarcal@gmail.com>
2016-04-01 20:02:38 -0300

    Adição do detalhamento do convenio

--- www/components/detalhe/detalheController.js
+++ www/components/detalhe/detalheController.js
@@ -3,8 +3,27 @@ myApp.controller('DetalheController', function($scope, $timeout , $http, $locati
 	//Parametros
 	$scope.params = $routeParams;
 
-	$scope.convenio = parseInt($scope.params.convenioId);
+	ADDRESS= 'http://74.124.24.115:8080'
 
-	
+	// Requisition Estados
+	$scope.requisitionDetail = function() {
+
+				requisicaoFactory.getRequest(ADDRESS+'/hackathon/ConveniosProgramasFTS?filter={NR_CONVENIO:'+parseInt($scope.params.convenioId)+'}').then(function(result) {
+				$scope.convenio = angular.fromJson(result._embedded["rh:doc"])[0];
+
+			}, function(reason) {
+			     alert("Erro ver console!")
+			    console.log("reason:", reason);
+			    // util._error(reason.data, reason.status, reason.headers, reason.config, $scope);
+			}, function(update) {
+			    console.log("update:", update);
+			})
+	}
+
+	$scope.requisitionDetail();
+
+	$scope.checkDate = function (date) {
+		return (new Date() < new Date(date));
+	}
 
 });
