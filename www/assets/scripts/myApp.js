bccda972f4487ed37bf503f49a9c41904d44cf4d
Thiago Marçal <thiagormarcal@gmail.com>
2016-04-11 18:48:02 -0300

    Acerto Grafico Home

--- www/assets/scripts/myApp.js
+++ www/assets/scripts/myApp.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute', 'mobile-angular-ui', 'angular-svg-round-progressbar', 'chart.js','tc.chartjs','ngCordova', 'angular-simple-chat', 'ngMeta', 'ngPercentDisplay', '720kb.tooltips']);
+var myApp = angular.module('myApp', ['ngRoute', 'mobile-angular-ui', 'angular-svg-round-progressbar', 'chart.js','tc.chartjs','ngCordova', 'angular-simple-chat', 'ngMeta', '720kb.tooltips', 'ngPercentDisplay']);
 myApp.config(function($routeProvider) {
     $routeProvider
     .when('/', {
